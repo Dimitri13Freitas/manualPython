8ba429d337d61b34079cd60223e073820fe70dc9
Dimitri Freitas <freitasdimitri7@gmail.com>
2022-09-24 02:19:45 -0300

    menu mobile finalizado funcionando

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 // Change Theme
 const btnTheme = document.querySelector('[data-btn="theme"]');
+const menuMobile = document.querySelector('[data-menu="lista"]');
+const btnMenu = document.querySelector('[data-btn="mobile"]');
 
 btnTheme.addEventListener('click', elemento);
+btnMenu.addEventListener('click', elemento);
 
 const ativa = {
   element:document,
-  changeTheme(value) {
+  changeTheme() {
     this.element.documentElement.classList.toggle('theme');
     btnTheme.classList.toggle('ativo');
   },
+  openMenu() {
+    menuMobile.classList.toggle('ativo');
+    btnMenu.classList.toggle('ativo');
+  }
 };
 
 function elemento(event) {
   const nome = event.target.name;
-  const valor = event.target.value;
-  ativa[nome](valor);
+  ativa[nome]();
 };
-
-// if
